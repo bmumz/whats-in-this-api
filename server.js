2e6fb091084d2ee4d177d3efcf5b496ced691ba5
bmumz <brandimummery@gmail.com>
2020-08-25 16:08:34 -0400

    removed a console.log

--- server.js
+++ server.js
@@ -18,12 +18,6 @@ const db = knex({
   },
 });
 
-db.select("*")
-  .from("users")
-  .then((data) => {
-    console.log(data);
-  });
-
 const app = express();
 app.use(express.json());
 app.use(cors());
