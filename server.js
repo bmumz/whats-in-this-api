19f363d896d2bc687dedd8eeb2a35e9e270cc369
bmumz <brandimummery@gmail.com>
2020-08-26 11:08:04 -0400

    adding database connection.

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-round-93422",
-    user: "",
-    password: "",
-    database: "whatsinthis",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
